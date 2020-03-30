08c023dad1a755c808bcbed8abd79d3324ff86be
Nelsoniwe <texno090@gmail.com>
2020-03-30 18:11:01 +0300

    labe_3.1

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace laba_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ListOptions options = new ListOptions();
+
+            Random ran = new Random();
+
+            List<string> StringList = new List<string>();
+
+            options.AddList(StringList,"asdf",0);
+            options.AddList(StringList, "asdf",0);
+            options.AddList(StringList, "asdf", 0);
+            options.AddList(StringList, "asdf", 1);
+            options.AddList(StringList, "asdf", 2);
+
+
+            foreach (var item in StringList)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+        }
+    }
+
+    class ListOptions
+    {
+        public void AddList(List<string> ExampleList,string word,int index)
+        {
+
+            if (ExampleList.Count>1&&index<ExampleList.Count&&index!=0)
+            {
+                string exampleword = ExampleList[index-1] + " " + ExampleList[index];
+
+                ExampleList.Insert(index, exampleword);
+            }
+            else if(ExampleList.Count==0||ExampleList.Count==1)
+            {
+                ExampleList.Add(word);
+            }
+            else if(index<0)
+            {
+                Console.WriteLine("wrong index");
+            }
+            else if(index>=ExampleList.Count)
+            {
+                Console.WriteLine("AAAAAAAAAAA");
+            }
+            
+
+        }
+
+
+
+
+    }
+
+
+
+}
